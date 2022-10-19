c05cc2eee1f55985fd1a3405cadc52c31d143637
Emiliano Acerbi <eacerbip@gmail.com>
2022-10-19 12:20:07 -0300

    primary input component

---
+++ components/PrimaryInput.js
@@ -0,0 +1,28 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const PrimaryInput = ({ label, placeholder }) => {
+  return (
+    <label>
+      <h3
+        style={{
+          fontSize: '15px',
+          fontWeight: '500',
+          color: '#494949'
+        }}
+      >
+        {label} <span style={{ color: '#FE645E' }} >*</span>
+      </h3>
+      <TextField
+        style={{
+          width: '436px',
+          height: '48px'
+        }}
+        placeholder={placeholder}
+        className='inputRounded'
+      />
+    </label>
+  )
+}
+
+export default PrimaryInput
