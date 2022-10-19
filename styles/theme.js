63df65865779edc1bf13aa0fa42ac56a646141ff
Emiliano Acerbi <eacerbip@gmail.com>
2022-10-19 10:24:04 -0300

    config file and test

---
+++ styles/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FE645E'
+    },
+    secondary: {
+      main: '#6E314A'
+    },
+    neutral: {
+      main: '#141E7A',
+      dark: '#0A1047'
+    }
+
+  }
+})
