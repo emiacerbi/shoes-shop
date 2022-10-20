8200a59e046e1a2cec942401a9cc9dd1f5fcd1af
Brisa Rojas <brojas@solvd.com>
2022-10-20 16:45:55 -0300

    formated code

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,15 @@ import Link from 'next/link'
 export default function Home () {
   return (
     <>
+      <h1>Links to other pages</h1>
       <Link href="/forgot-password">
         <a style={{ color: 'black' }}>Forgot Password</a>
       </Link>
-
       <br/>
       <Link href="/reset-password">
         <a style={{ color: 'black' }}>Reset Password</a>
       </Link>
+
     </>
-
-  // <div>
-  //   <Heading headingText='Probando...' />
-  //   <StyledText text='Probando...' />
-  //   <Form onSubmit={(e) => e.preventDefault()||console.log('submit done')}>
-  //     <PrimaryInput label='Password' placeholder='at least 8 characters' />
-  //     <PrimaryInput label='Confirm Password' placeholder='at least 8 characters' />
-  //     <PrimaryButton>
-  //       Reset Password
-  //     </PrimaryButton>
-  //   </Form>
-
-  // </div>
   )
 }
