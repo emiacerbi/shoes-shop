34daa27894f50507bb79d2734f74e0ea98e48829
Brisa Rojas <brojas@solvd.com>
2022-10-20 12:39:18 -0300

    es-lint-config

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,41 +1,44 @@
-import Form from "../src/components/Form";
-import PrimaryButton from "../src/components/PrimaryButton";
-import PrimaryInput from "../src/components/PrimaryInput";
-import Heading from "../src/components/Heading/heading";
-import StyledText from "../src/components/StyledText/StyledText";
-import Header from "../src/components/Header/Header";
-import Link from "next/link";
+import Form from '../src/components/Form/Form'
+import PrimaryButton from '../src/components/PrimaryButton/PrimaryButton'
+import PrimaryInput from '../src/components/PrimaryInput/PrimaryInput'
+import Heading from '../src/components/Heading/heading'
+import StyledText from '../src/components/StyledText/StyledText'
+import Header from '../src/components/Header/Header'
+import Link from 'next/link'
 
-import styles from "../styles/forgotpassword.module.css";
+import styles from '../styles/forgotpassword.module.css'
 
-export default function ForgotPassword() {
+export default function ForgotPassword () {
   return (
-    <div className={styles["forgot-password-page"]}>
-      <Header className={styles['forgot-password-header']}/>
-      <main className={styles["forgot-password-main"]}>
-        <Heading headingText="Forgot Password?" />
+    <div className={styles['forgot-password-page']}>
+      <Header className={styles['forgot-password-header']} />
+      <main className={styles['forgot-password-main']}>
+        <Heading headingText='Forgot Password?' />
 
-        <StyledText text="Don’t worry, we’ll send you reset instructions." />
+        <StyledText text='Don’t worry, we’ll send you reset instructions.' />
 
         <Form onSubmit={(e) => e.preventDefault()}>
           <PrimaryInput
-            type="email"
-            label="Email"
-            placeholder="example@mail.com"
+            type='email'
+            label='Email'
+            placeholder='example@mail.com'
           />
-          <PrimaryButton
-            type="submit"
-            children="Reset password"
-          ></PrimaryButton>
+          <PrimaryButton type='submit'>Reset password</PrimaryButton>
         </Form>
 
-        <Link href="/index.js" >
-          <a className={styles['forgot-password__go-back']} >Back to log in</a>
+        <Link href='/index.js'>
+          <a className={styles['forgot-password__go-back']}>Back to log in</a>
         </Link>
       </main>
-      <aside className={styles["forgot-password-aside"]}>
-        <img src="/shoes.png" alt="shoe pic" className={styles["forgot-password-aside__image"]}/>
+      <aside className={styles['forgot-password-aside']}>
+        <div>
+          <img
+            src='/shoes.png'
+            alt='shoe pic'
+            className={styles['forgot-password-aside__image']}
+          />
+        </div>
       </aside>
     </div>
-  );
+  )
 }
