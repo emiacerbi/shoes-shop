cfd1f9dd2890d0d4cbb691824aeb9063ba72d0b9
Brisa Rojas <brojas@solvd.com>
2022-10-21 11:02:28 -0300

    resolve conflict

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home () {
   return (
 
-    <Link href="/sign-up">
+    <Link href="/forgot-password">
       <a style={{ color: 'black' }}>Forgot Password</a>
     </Link>
 
