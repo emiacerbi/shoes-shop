00794b8aa64cbb8212434361b380aaba02a5ed83
JulianOvie <julianovie234@gmail.com>
2022-10-22 10:46:40 -0300

    fix styles

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,13 +5,13 @@ import styles from './styles.module.css'
 export default function ProductCard ({ productTitle, productDescription, productPrice }) {
   return (
     <div className={styles['product-card-container']}>
-      <Image src="/shoes.png" width={150} height={150} alt="product"/>
+      <Image src="/shoes.png" layout='fixed' width={150} height={180} alt="product"/>
       <div className={styles['product-card-description']}>
-        <div className={styles['product-card-description__titles']}>
-          <h1>{productTitle}</h1>
+        <div>
+          <h3 className={styles.h3}>{productTitle}</h3>
           <p className={styles.p}>{productDescription}</p>
         </div>
-        <h1 className={styles['product-card-description__price']}>${productPrice}</h1>
+        <h3 className={styles.h3}>${productPrice}</h3>
       </div>
     </div>
   )
