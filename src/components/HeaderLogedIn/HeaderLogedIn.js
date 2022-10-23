a4b094602af4920feea6cd12dbeda7716073ecab
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-10-23 12:06:57 -0300

    delete unnecessary lines

--- src/components/HeaderLogedIn/HeaderLogedIn.js
+++ src/components/HeaderLogedIn/HeaderLogedIn.js
@@ -6,36 +6,22 @@ import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
-// import Container from '@mui/material/Container'
-// import Avatar from '@mui/material/Avatar'
-// import Button from '@mui/material/Button'
-// import Tooltip from '@mui/material/Tooltip'
 import MenuItem from '@mui/material/MenuItem'
-// import AdbIcon from '@mui/icons-material/Adb'
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
 import styles from './styles.module.css'
 const pages = ['Home', 'For women', 'For Men', 'Accesories', 'Sale']
 const links = ['/', '/for-women', '/for-men', '/accesories', '/sale']
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
 
 function HeaderLogedIn () {
   const [anchorElNav, setAnchorElNav] = React.useState(null)
-  const [anchorElUser, setAnchorElUser] = React.useState(null)
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
   }
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget)
-  }
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
   }
 
   return (
@@ -77,11 +63,9 @@ function HeaderLogedIn () {
 
         {/* Mobile  */}
         <Box sx={{ mt: '20px', flexGrow: 1, display: { xs: 'flex', sm: 'none' }, justifyContent: 'space-between', alignItems: 'center' }}>
-
           <Link href={'/'} sx={{ mt: 'auto' }}>
             <img src="/logo.png" alt="logo" className={styles.logo} />
           </Link>
-
           <Box sx={{ maxWidth: '150px', width: '100%', mr: 2, display: 'flex', alignItems: 'center', justifyContent: 'space-evenly' }}>
             <Link href={'/cart'}>
               <LocalMallOutlinedIcon sx={{ color: '#292D32' }}/>
@@ -127,74 +111,10 @@ function HeaderLogedIn () {
                 </MenuItem>
               )
               )}
-
             </Menu>
           </Box>
         </Box>
-        {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-        {/* <Typography
-          variant="h5"
-          noWrap
-          component="a"
-          href=""
-          sx={{
-            mr: 2,
-            display: { xs: 'flex', md: 'none' },
-            flexGrow: 1,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none'
-          }}
-        >
-            LOGO
-        </Typography> */}
-
-        {/* <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              {page}
-            </Button>
-          ))}
-        </Box>
-
-        <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">{setting}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-
-        </Box> */}
       </Toolbar>
-      {/* </Container> */}
       <div className={styles.separationLine}></div>
     </AppBar>
   )
