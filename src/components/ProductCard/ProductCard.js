06a755aa8dce8334f3c8dc3db11145024e6ae3c8
JulianOvie <julianovie234@gmail.com>
2022-10-25 02:29:18 -0300

    add: styles for desktop UI of productCard & SubHeader component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export default function ProductCard ({ productTitle, productDescription, productPrice }) {
   return (
     <div className={styles['product-card-container']}>
-      <Image src="/shoes.png" layout='fixed' width={150} height={180} alt="product"/>
+      <Image src="/shoes.png" layout='responsive' width={150} height={180} alt="product"/>
       <div className={styles['product-card-description']}>
         <div>
           <h3 className={styles.h3}>{productTitle}</h3>
