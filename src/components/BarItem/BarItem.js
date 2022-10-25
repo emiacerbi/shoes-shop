082fafaa817e469faa2578ae277e08f75e439ec0
Brisa Rojas <brojas@solvd.com>
2022-10-25 15:30:35 -0300

    changed classes name

--- src/components/BarItem/BarItem.js
+++ src/components/BarItem/BarItem.js
@@ -1,11 +1,11 @@
 import styles from './styles.module.css'
 
-export default function SideBarItem ({ name, notifNumber, children }) {
+export default function BarItem ({ name, num, children }) {
   return (
-    <div className={styles['side-bar-item']}>
+    <div className={styles['bar-item']}>
       {children}
-      <p className={styles['side-bar-item__name']}>{name}</p>
-      {notifNumber ? <div className={styles['side-bar-item__notif-num']}> <p >{notifNumber}</p> </div> : null}
+      <p className={styles['bar-item__name']}>{name}</p>
+      {num ? <div className={styles['bar-item__notif-num']}> <p >{num}</p> </div> : null}
     </div>
   )
 }
