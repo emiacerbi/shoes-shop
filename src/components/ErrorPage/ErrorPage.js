819ff929505946e0562c1e97536235354d552ed3
dazuaje <davidazuaje1084@gmail.com>
2022-11-01 10:33:24 -0300

    Fixing responsive design and deleting css module files

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -14,35 +14,27 @@ const ErrorPage = () => {
         width: '100%',
         position: 'relative',
         '@media (min-width: 1000px)': {
-          border: '2px solid black',
           maxHeight: '80vh',
           flexDirection: 'row'
-
         }
-
       }}
     >
-
       <Box
+
         sx={{
           display: 'flex',
           alignItems: 'center',
-          // width: '430px',
-          minHeight: '100vh', // 50 Update
+          minHeight: '100vh',
           flexDirection: 'column',
-          // width: '430px',
           width: '100px',
-
-          border: '5px solid yellow',
           flex: '1',
           '@media (min-width: 1000px)': {
             minHeight: '79vh',
             width: '50%'
-
           }
+        }}
 
-        }}>
-
+      >
         <Box
           sx={{
             display: 'flex',
@@ -51,43 +43,55 @@ const ErrorPage = () => {
             paddingBlock: '2.5rem',
             paddingInline: '3.5rem',
             textAlign: 'center',
-            border: '2px solid red',
             alignItems: 'center',
             gap: '.5rem',
             width: '100%',
-            // width: '420px',
-
             '@media (min-width: 1000px)': {
               justifyContent: 'center',
               alignItems: 'flex-start',
               gap: '1rem',
               textAlign: 'start'
-
             }
-          }}>
+          }}
+        >
+          <Typography variant="h1">Error 404</Typography>
+          <Typography
+            variant="p"
+            sx={{
+              maxWidth: '390px',
+              display: 'none',
+              '@media (min-width: 1000px)': { display: 'flex' }
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna
+          </Typography>
 
-          <Typography variant='h1'>Error 404</Typography>
-          <Typography variant='p'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</Typography>
+          <Typography
+            sx={{ '@media (min-width: 1000px)': { display: 'none' } }}
+            variant="p"
+          >
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam{' '}
+          </Typography>
 
           <Box
             sx={{
               display: 'flex',
               marginTop: 'auto',
-              marginInline: 'auto',
               width: '300px',
               gap: '1rem',
               '@media (min-width: 1000px)': {
                 marginTop: '0',
                 margin: '0'
               }
-            }}>
-            <Link href='/'>
+            }}
+          >
+            <Link href="/">
               <SecondaryButton>Go Back</SecondaryButton>
             </Link>
-            <Link href='/home'>
-              <PrimaryButton maxWidth='152px'>Home</PrimaryButton>
+            <Link href="/home">
+              <PrimaryButton maxWidth="152px">Home</PrimaryButton>
             </Link>
-
           </Box>
         </Box>
       </Box>
@@ -95,13 +99,11 @@ const ErrorPage = () => {
         sx={{
           backgroundColor: '#e5e6e7',
           backgroundImage: "url('/error404.png')",
-          border: '2px solid blue',
           backgroundPosition: 'bottom',
           backgroundSize: 'contain',
           backgroundRepeat: 'no-repeat',
           position: 'absolute',
-          height: '80vh',
-          // minHeight: { sm: '55vh', xs: '40vh', md: '60vh' },
+          height: '75%',
           width: '100%',
           top: '0',
           borderRadius: '0 0 2rem 2rem',
@@ -113,8 +115,8 @@ const ErrorPage = () => {
             backgroundSize: 'contain',
             flexDirection: 'column'
           }
-
-        }}></Box>
+        }}
+      ></Box>
       <Box
         sx={{
           display: 'none',
@@ -128,8 +130,8 @@ const ErrorPage = () => {
             display: 'block',
             backgroundPosition: 'center'
           }
-        }}></Box>
-
+        }}
+      ></Box>
     </Box>
   )
 }
