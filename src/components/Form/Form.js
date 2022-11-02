11409c9f8099aacc7a3486df587b9f1fc97daf00
Emiliano Acerbi <eacerbip@gmail.com>
2022-11-02 09:57:24 -0300

    refactor to MUI

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,20 @@
-import styles from './style.module.css'
+import { FormGroup } from '@mui/material'
 
 const Form = ({ onSubmit, children }) => {
   return (
-    <form
-      className={styles.form}
+    <FormGroup
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        paddingBlock: '2.5rem',
+        gap: '1.5rem',
+        width: '100%',
+        maxWidth: '436px'
+      }}
       onSubmit={onSubmit}
     >
       {children}
-    </form>
+    </FormGroup>
   )
 }
 
