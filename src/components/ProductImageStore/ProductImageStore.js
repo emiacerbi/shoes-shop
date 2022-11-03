35dd0f2329d78ebeec8f28b5e917d551974c31fe
Julian <julianovie234@gmail.com>
2022-11-03 12:07:52 -0300

    replace label for FormLabel

--- src/components/ProductImageStore/ProductImageStore.js
+++ src/components/ProductImageStore/ProductImageStore.js
@@ -1,4 +1,4 @@
-import { Box, Container, Typography, Link, Input } from '@mui/material'
+import { Box, Container, Typography, Link, Input, FormLabel } from '@mui/material'
 import PhotoCameraBackIcon from '@mui/icons-material/PhotoCameraBack'
 import Image from 'next/image'
 
@@ -9,9 +9,9 @@ export default function ProductImageStore () {
         <Typography variant='h3' sx={{ fontSize: '15px !important', color: '#494949', mb: '1.2rem' }}>Product Images</Typography>
         <Box sx={{ display: 'flex', overflow: 'auto', maxWidth: 740, maxHeight: 550, flexWrap: 'wrap', gap: '50px', justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 320, height: 380, border: '1px dashed #5C5C5C' }}>
-            <label for='srcImg'>
+            <FormLabel for='srcImg'>
               <PhotoCameraBackIcon sx={{ color: '#6E7278', fontSize: '100px', cursor: 'pointer' }}/>
-            </label>
+            </FormLabel>
             <Input id='srcImg' type='file' sx={{ display: 'none' }}></Input>
             <Typography variant='p' align="center">
                 Drop your image here, or select <Link href="//www.google.com">click here to browse</Link>
