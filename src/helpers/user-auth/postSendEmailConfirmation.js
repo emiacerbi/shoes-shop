9c9a5eb462f6c10788271def46f2781471c55f01
Emiliano Acerbi <eacerbip@gmail.com>
2022-11-07 13:52:50 -0300

    refactored some methods

--- src/helpers/user-auth/postSendEmailConfirmation.js
+++ src/helpers/user-auth/postSendEmailConfirmation.js
@@ -1,20 +1,11 @@
 import { fetchWrapper } from '../fetchWrapper'
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-const ENDPOINT = `${BASE_URL}/api/auth/send-email-confirmation`
-
 export const postResetPassword = async (email) => {
-  const config = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    },
-    body: JSON.stringify({
+  const response = await fetchWrapper('api/auth/send-email-confirmation', {
+    body: {
       email
-    })
-  }
-
-  const response = await fetchWrapper(ENDPOINT, config)
+    }
+  })
   return response
 }
 
