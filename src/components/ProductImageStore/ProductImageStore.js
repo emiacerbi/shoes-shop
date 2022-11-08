2a642e89314905ddd2807fdcd6bb518abb0f8532
JulianOvie <julianovie234@gmail.com>
2022-11-08 14:57:44 -0300

    replace htmlFor instead for keyword

--- src/components/ProductImageStore/ProductImageStore.js
+++ src/components/ProductImageStore/ProductImageStore.js
@@ -9,7 +9,7 @@ export default function ProductImageStore () {
         <Typography variant='h3' sx={{ fontSize: '15px !important', color: '#494949', mb: '1.2rem' }}>Product Images</Typography>
         <Box sx={{ display: 'flex', overflow: 'auto', maxWidth: 740, maxHeight: 550, flexWrap: 'wrap', gap: '50px', justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 320, height: 380, border: '1px dashed #5C5C5C' }}>
-            <FormLabel for='srcImg'>
+            <FormLabel htmlFor='srcImg'>
               <PhotoCameraBackIcon sx={{ color: '#6E7278', fontSize: '100px', cursor: 'pointer' }}/>
             </FormLabel>
             <Input id='srcImg' type='file' sx={{ display: 'none' }}></Input>
