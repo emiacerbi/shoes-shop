69d6a1f571123e130fa439d00af238f0ab3f1389
JulianOvie <julianovie234@gmail.com>
2022-11-07 21:16:33 -0300

    use Image component instead background-image prop

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,7 @@ import AccountCircleOutlined from '@mui/icons-material/AccountCircleOutlined'
 import LogoutIcon from '@mui/icons-material/Logout'
 import { Box, Button, Typography, useTheme } from '@mui/material'
 import { UserContext } from 'context/UserContext'
+import Image from 'next/image'
 import Link from 'next/link'
 import { signOut } from 'next-auth/react'
 
@@ -45,7 +46,9 @@ export default function Home () {
           </Box>
         </Box>
         <Box component='main' sx={{ m: '20px', width: '100%' }}>
-          <Box sx={{ height: { xs: 132, sm: 260 }, m: { xs: '-20px', sm: 0 }, backgroundImage: "url('/bgHomeImg.png')", backgroundSize: 'cover' }}></Box>
+          <Box sx={{ width: '100%', height: { xs: 132, sm: 260 }, m: { xs: '-20px', sm: 0 }, position: 'relative' }}>
+            <Image src='/bgHomeImg.png' alt='panelImage' layout='fill' ></Image>
+          </Box>
           <SubHeader/>
           <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <Typography variant='h1'>My Products</Typography>
