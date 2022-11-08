2c90e404a1baf096c562d2d8ff91cc74fd9a319e
JulianOvie <julianovie234@gmail.com>
2022-11-08 14:58:29 -0300

    add SelectInput component

---
+++ src/components/SelectInput/SelectInput.js
@@ -0,0 +1,23 @@
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography, useTheme } from '@mui/material'
+const SelectInput = ({ inputValues, name, onChange }) => {
+  const theme = useTheme()
+  return (
+
+    <Box sx={{ width: '100%', display: 'flex', gap: '.5rem', flexDirection: 'column' }}>
+      <Typography variant='p' sx={{ fontWeight: '500' }} >
+        {name}  <Box component='span' sx={{ color: theme.palette.primary.main }}>*</Box></Typography>
+      <FormControl fullWidth>
+        <InputLabel id={`select-input-${name}`}>{name}</InputLabel>
+        <Select
+          labelId={`select-input-${name}`}
+          id="select"
+          label={name}
+        >
+          {inputValues.map((value, i) => <MenuItem key={i} value={value}>{value}</MenuItem>)}
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
+
+export default SelectInput
