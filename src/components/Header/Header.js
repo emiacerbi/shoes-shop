cd8aed289350c6686d9d7703dfd0bfdab1c47106
Brisa Rojas <brojas@solvd.com>
2022-11-08 20:00:32 -0300

    fixes in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,8 @@ export default function Header () {
         justifyContent: 'flex-start',
         alignItems: 'center',
         borderBottom: { sx: 'none', sm: '1px solid #eaecf0' },
-        padding: '20px'
+        height: { sm: '68px' },
+        paddingLeft: { sm: '20px' }
       }}
     >
       <Box
