1f61fafd187abdb95e33ec47c7401be8e839fafd
JulianOvie <julianovie234@gmail.com>
2022-11-10 17:32:18 -0300

    fix styles home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import HeaderLoggedIn from '@components/HeaderLoggedIn/HeaderLoggedIn'
 import BarItem from '@components/NavBarItem/NavBarItem'
 import PrimaryButton from '@components/PrimaryButton/PrimaryButton'
@@ -27,15 +27,8 @@ export async function getStaticProps () {
 export default function Home ({ products }) {
   console.log(products)
   const context = useContext(UserContext)
-  const [ScreenWidth, setScreenWidth] = useState()
 
   console.log(context, 'USER INFO')
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setScreenWidth(window.innerWidth)
-    }
-  }, [])
 
   const theme = useTheme()
 
@@ -58,20 +51,24 @@ export default function Home ({ products }) {
             </Button>
           </Box>
         </Box>
-        <Box component='main' sx={{ m: '20px', width: '100%' }}>
-          <Box sx={{ height: { xs: 132, sm: 260 }, m: { xs: '-20px', sm: 0 }, position: 'relative' }}>
+        <Box component='main' sx={{ m: { xs: 0, sm: '20px' } }}>
+          <Box sx={{ width: '100%', height: { xs: 132, sm: 260 }, mb: '-30px', position: 'relative' }}>
             <Image src='/bgHomeImg.png' alt='panelImage' layout='fill' ></Image>
           </Box>
-          <SubHeader name = { context?.user?.userInfo?.username ? context?.user?.userInfo?.username : <Skeleton variant="rectangular" width={100} height={20} />}/>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Typography variant='h1'>My Products</Typography>
-            {ScreenWidth > 599 && (<PrimaryButton maxWidth='152px' ><Link href='/add-product'>Add Products</Link></PrimaryButton>)}
-          </Box>
-          <Box sx={{ mt: '20px', display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', gap: '20px' }}>
-            <ProductCard image={'/airmax-270.png'} productTitle="Nike AirMax 90" productPrice="140" productDescription="Men's Shoes"/>
-            <ProductCard image={'/shoes.png'} productTitle="Nike AirMax 270" productPrice="160" productDescription="Women's Shoes"/>
-            <ProductCard image={'/air-force.png'} productTitle="Nike Air Force 1 '07 SE" productPrice="110" productDescription="Women's Shoes"/>
-            <ProductCard image={'/air-zoom-pegasus.png'} productTitle="Nike Air Zoom Pegasus" productPrice="120" productDescription="Men's Shoes"/>
+          <Box sx={{ width: '100%', p: '20px' }}>
+            <SubHeader name = { context?.user?.userInfo?.username ? context?.user?.userInfo?.username : <Skeleton variant="rectangular" width={100} height={20} />}/>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant='h1'>My Products</Typography>
+              <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                <PrimaryButton maxWidth='152px' ><Link href='/add-product'>Add Products</Link></PrimaryButton>
+              </Box>
+            </Box>
+            <Box sx={{ mt: '20px', display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', gap: '20px' }}>
+              <ProductCard image={'/airmax-270.png'} productTitle="Nike AirMax 90" productPrice="140" productDescription="Men's Shoes"/>
+              <ProductCard image={'/shoes.png'} productTitle="Nike AirMax 270" productPrice="160" productDescription="Women's Shoes"/>
+              <ProductCard image={'/air-force.png'} productTitle="Nike Air Force 1 '07 SE" productPrice="110" productDescription="Women's Shoes"/>
+              <ProductCard image={'/air-zoom-pegasus.png'} productTitle="Nike Air Zoom Pegasus" productPrice="120" productDescription="Men's Shoes"/>
+            </Box>
           </Box>
         </Box>
       </Box>
