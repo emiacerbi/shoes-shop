bbf9f4a58e3d6a4bea5b290c4410b5edc3cda159
Brisa Rojas <brojas@solvd.com>
2022-11-11 14:04:15 -0300

    use of defaults

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -2,12 +2,11 @@ import axios from 'axios'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
-const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 1000,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
+const axiosInstance = axios.create()
+
+axiosInstance.defaults.baseURL = BASE_URL
+axiosInstance.defaults.headers = {
+  'Content-Type': 'application/json'
+}
 
 export default axiosInstance
