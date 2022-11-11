053cfeeb7cb52142044704c18cff2ef52cabe5e9
Emiliano Acerbi <eacerbip@gmail.com>
2022-11-11 18:29:40 -0300

    red getUserInfo

--- src/helpers/user-auth/getUserInfo.js
+++ src/helpers/user-auth/getUserInfo.js
@@ -1,7 +1,12 @@
 import axiosInstance from '../../axios/axiosInstance'
 
 export const getUserInfo = async (token) => {
-  const response = await axiosInstance.get('/api/users/me', { token })
+  const response = await axiosInstance.get('/api/users/me', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
   return response
 }
 
