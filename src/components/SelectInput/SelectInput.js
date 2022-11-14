398a22456c667dca220a13d9f40d4d3e3fc5fc3e
Emiliano Acerbi <eacerbip@gmail.com>
2022-11-14 12:03:25 -0300

    modified input

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -1,17 +1,17 @@
 import { Box, FormControl, InputLabel, MenuItem, Select, Typography, useTheme } from '@mui/material'
-const SelectInput = ({ inputValues, name, onChange }) => {
+const SelectInput = ({ inputValues, name, onChange, label }) => {
   const theme = useTheme()
   return (
 
     <Box sx={{ width: '100%', display: 'flex', gap: '.5rem', flexDirection: 'column' }}>
       <Typography variant='p' sx={{ fontWeight: '500' }} >
-        {name}  <Box component='span' sx={{ color: theme.palette.primary.main }}>*</Box></Typography>
+        {label}  <Box component='span' sx={{ color: theme.palette.primary.main }}>*</Box></Typography>
       <FormControl fullWidth>
-        <InputLabel id={`select-input-${name}`}>{name}</InputLabel>
+        <InputLabel id={`select-input-${name}`}>{label}</InputLabel>
         <Select
           labelId={`select-input-${name}`}
           id="select"
-          label={name}
+          label={label}
           name={name}
           onChange={onChange}
           defaultValue=''
