a667ce212441c5a9fdd5cd68c928cb8e46d987d6
Brisa Rojas <brojas@solvd.com>
2022-11-15 16:28:28 -0300

    undo changes to include label

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -1,16 +1,17 @@
-import { Box, FormControl, MenuItem, Select, Typography } from '@mui/material'
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 const SelectInput = ({ inputValues, name, onChange, label }) => {
   return (
     <Box sx={{ width: '100%', display: 'flex', gap: '.5rem', flexDirection: 'column' }}>
       <Typography variant='p' sx={{ fontWeight: '500' }} >
-        {label}  <Box component='span' sx={{ color: 'primary.main' }}>*</Box></Typography>
+        {label}  <Box component='span' sx={{ color: 'primary.main' }}>*</Box>
+      </Typography>
       <FormControl fullWidth>
-        {/* <InputLabel id={`select-input-${name}`}>{label}</InputLabel> */}
+        <InputLabel id={`select-input-${name}`}>{label}</InputLabel>
         <Select
           labelId={`select-input-${name}`}
           id="select"
-          // label={label}
-          // name={name}
+          label={label}
+          name={name}
           onChange={onChange}
           defaultValue=''
           required
