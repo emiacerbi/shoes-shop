f4dd2ee442ebcbc090dbbae16902d973be108eb5
Brisa Rojas <brojas@solvd.com>
2022-11-15 11:59:36 -0300

    add missing input label

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -1,11 +1,9 @@
-import { Box, FormControl, InputLabel, MenuItem, Select, Typography, useTheme } from '@mui/material'
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 const SelectInput = ({ inputValues, name, onChange, label }) => {
-  const theme = useTheme()
   return (
-
     <Box sx={{ width: '100%', display: 'flex', gap: '.5rem', flexDirection: 'column' }}>
       <Typography variant='p' sx={{ fontWeight: '500' }} >
-        {label}  <Box component='span' sx={{ color: theme.palette.primary.main }}>*</Box></Typography>
+        {label}  <Box component='span' sx={{ color: 'primary.main' }}>*</Box></Typography>
       <FormControl fullWidth>
         <InputLabel id={`select-input-${name}`}>{label}</InputLabel>
         <Select
