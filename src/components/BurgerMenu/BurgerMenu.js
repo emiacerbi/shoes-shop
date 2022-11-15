8d629d7118350b9d5c5d742da8c1368aa376d4c7
JulianOvie <julianovie234@gmail.com>
2022-11-15 12:16:53 -0300

    add logOut in every page on burgerMenu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
+import BarItem from '@components/NavBarItem/NavBarItem'
+import LogoutIcon from '@mui/icons-material/Logout'
 import MenuIcon from '@mui/icons-material/Menu'
+import { Box } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import Link from '@mui/material/Link'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Typography from '@mui/material/Typography'
+import { signOut } from 'next-auth/react'
 
 function BurgerMenu ({ pages, links }) {
   const [anchorElNav, setAnchorElNav] = useState(null)
@@ -50,6 +54,7 @@ function BurgerMenu ({ pages, links }) {
         }}
       >
         {/* Links menu pages */}
+
         {pages.map((page, index) => (
           <MenuItem key={page} onClick={handleCloseNavMenu}>
             <Link sx={{ textDecoration: 'none', color: '#000000' }} href={`${links[index]}`}>
@@ -58,6 +63,9 @@ function BurgerMenu ({ pages, links }) {
           </MenuItem>
         )
         )}
+        <MenuItem>
+          <Typography onClick={() => signOut()} textAlign="center">Log out</Typography>
+        </MenuItem>
       </Menu>
     </>
   )
