13e8ee353b97fb175e067a61aa2dd3d0effa9ce9
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-11-20 02:28:24 -0300

    add next/router to URL

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,18 +1,39 @@
+import { useEffect, useState } from 'react'
 import { Checkbox, FormControlLabel, FormGroup } from '@mui/material'
+import { useRouter } from 'next/router'
 
-function CheckBox ({ label }) {
+function CheckBox({ label }) {
+  const [value, setValue] = useState('')
+  // const [checked, setChecked] = useState(false)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(
+      {
+        pathname: '/search-results'
+      },
+      `/search-results/${value}`,
+      { shallow: true }
+    )
+  }, [value])
+
   return (
     <>
-      <FormGroup aria-label="position" column >
-        {
-          label.map(data => (
-            <>
-              <FormControlLabel
-                label={data}
-                control={<Checkbox/>}/>
-            </>
-          ))
-        }
+      <FormGroup aria-label="position" column>
+        {label.map((data) => (
+          <>
+            <FormControlLabel
+              label={data}
+              control={
+                <Checkbox
+                  // onClick={() => setChecked(!checked)}
+                  onChange={() => setValue(data)}
+                />
+              }
+            />
+          </>
+        ))}
       </FormGroup>
     </>
   )
