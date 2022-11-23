b1cded83ca25e980a2ff06adbeae0a30d9d4d9b4
Emiliano Acerbi <eacerbip@gmail.com>
2022-11-23 20:59:49 -0300

    reducePrice without toString

--- src/helpers/reducePrice.js
+++ src/helpers/reducePrice.js
@@ -1,2 +1,2 @@
 export const reducePrice = (array) =>
-  array.reduce((acc, elem) => acc + elem.price.toString() * elem.quantity, 0)
+  array.reduce((acc, elem) => acc + elem.price * elem.quantity, 0)
